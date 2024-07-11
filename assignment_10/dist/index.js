09b007bc675395562af620c3135e5302b6929697
“Muhammad <muhammadmuzammil303120@gmail.com>
2024-07-11 12:50:52 +0500

    first commit

---
+++ assignment_10/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Author: Muhammad Muzammil
+// Date: 2024-07-11
+// This program increments and decrements a number and prints the results.
+// Initializing the number
+let num = 10;
+// Incrementing the number
+num++; // num is now 11
+console.log("After incrementing, the number is: " + num);
+// Decrementing the number
+num--; // num is now 10 again
+console.log("After decrementing, the number is: " + num);
---
+++ assignment_10/src/index.ts
@@ -0,0 +1,14 @@
+// Author: Muhammad Muzammil
+// Date: 2024-07-11
+// This program increments and decrements a number and prints the results.
+
+// Initializing the number
+let num: number = 10;
+
+// Incrementing the number
+num++; // num is now 11
+console.log("After incrementing, the number is: " + num);
+
+// Decrementing the number
+num--; // num is now 10 again
+console.log("After decrementing, the number is: " + num);
